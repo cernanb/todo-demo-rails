137b93ade6b3c325550c677566c908b64161722e
Cernan Bernardo <cernanb@gmail.com>
2018-02-11 22:16:01 -0800

    Add default value for completed column

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 20180212061234) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
