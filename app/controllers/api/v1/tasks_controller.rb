443001c5b3df7f9ecb4f63a0fa87fdb45073b56c
Cernan Bernardo <cernanb@gmail.com>
2018-02-11 22:37:50 -0800

    Add create action in TasksController

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -3,4 +3,14 @@ class Api::V1::TasksController < ApplicationController
     @tasks = Task.all
     render json: @tasks
   end
+
+  def create
+    @task = Task.new(task_params)
+    render json: @task
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:name)
+    end
 end
