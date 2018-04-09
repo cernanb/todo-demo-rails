8aca72d8ed92c7a0ba12d2ec17405e42e7ed90ac
Cernan Bernardo <cernanb@gmail.com>
2018-04-09 15:24:55 -0700

    Build user model and associate to task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180212061234) do
+ActiveRecord::Schema.define(version: 20180409222328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20180212061234) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.boolean "completed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
