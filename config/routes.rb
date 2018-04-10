4b33061dfdeb7346e84c1f842479ad69a0b5cb50
Cernan Bernardo <cernanb@gmail.com>
2018-04-09 17:46:48 -0700

    Add routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :tasks, only: [:index, :create, :update, :destroy]
+      resources :users, only: [:create]
     end
   end
 end
